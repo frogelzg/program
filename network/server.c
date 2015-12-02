3b76dea02df3aa936f0d89a1978aa5dbe1b651e4
Froge Liu <frogelzg@163.com>
2015-12-02 23:31:17 +0800

    Modify server.c to add function: save client information

--- network/server.c
+++ network/server.c
@@ -9,38 +9,24 @@
 #include <net/if.h>
 #include <arpa/inet.h>
 
-#define BACKLOG 5
-#define PORT    1234
-#define TRUE    1
-#define FALSE   0
-#define BUFF    1024
+#define BACKLOG     5
+#define PORT        1234
+#define TRUE        1
+#define FALSE       0
+#define BUFF        1024
+#define CLIENTLEN   10
 
-
-int bios_open_socket(void)
+struct client_addr
 {
-    int fd;
-    struct sockaddr_in addr;
+    char    ip_addr[15];
+    unsigned short port;
+}__attribute((packed, aligned(4)))__;
 
-    /* Create a socket */
-    fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if(fd < 0)
-        return -1;
-    
-    /* configure the server address */
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = INADDR_ANY;
-    addr.sin_port = htons(PORT);
+typedef struct client_addr client_addr;
 
-    /* Bind the address on the fd */
-    if(bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
-    {
-        perror("bind() error!");
-        return -1;
-    }
-
-    return fd;
-}
+int bios_open_socket(void);
+int add_client_addr(client_addr *client, char *address, unsigned short port);
+int show_client_addr(client_addr *client);
 
 int main()
 {
@@ -53,7 +39,10 @@ int main()
     /* a config struct */
     struct      ifreq ifr;
     char        *address;
-    int         port;
+    unsigned short  port;
+    client_addr client[CLIENTLEN];
+
+    memset(client, 0, sizeof(client));
 
     /* Create a socket */
     fd = bios_open_socket();
@@ -106,6 +95,7 @@ int main()
             port    = ntohs(addr.sin_port);
             printf("Client address：%s.\n", address);
             printf("Client port: %d.\n", port);
+            add_client_addr(client, address, port);
         }
         /* receive the data from client */
         size = read(ss, buff, BUFF);
@@ -138,6 +128,68 @@ int main()
     /* Remember that you must close all fd which have opend */
     close(fd);
     close(ss);
+
+    return 0;
+}
+
+int add_client_addr(client_addr *client, char *address, unsigned short port)
+{
+    int i;
+
+    for(i = 0; i < CLIENTLEN; i++)
+    {
+        if(!strcmp(client[i].ip_addr, address) && (client[i].port == port))
+        {
+            printf("Have same socket in addr array!\n");
+            return 0;
+        }
+    }
+
+    if(i >= CLIENTLEN)
+    {
+        printf("Over array!\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+int bios_open_socket(void)
+{
+    int fd;
+
+    struct sockaddr_in addr;
+    /* Create a socket */
+    fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(fd < 0)
+        return -1;
+    
+    /* configure the server address */
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+    /* Bind the address on the fd */
+    if(bind(fd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    {
+        perror("bind() error!");
+        return -1;
+    }
+
+    return fd;
+}
+
+int show_client_addr(client_addr *client)
+{
+    int i;
+
+    for(i = 0; i < CLIENTLEN; i++)
+    {
+        printf("%d.\n", i);
+        printf("ip address: %s.\n", client[i].ip_addr);
+        printf("port: %d.\n", client[i].port);
+    }
 
     return 0;
 }
